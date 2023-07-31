13fcd496a8c1a5cb4616f3df0bfa3edc4ddfec05
Ana-BSL <anabeatrizna43@gmail.com>
2023-07-31 11:13:58 -0300

    mudança estrutura

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Senha senha = new Senha();
-        senha.adicionar();
-        System.out.println("Senhas encontradas:");
-        senha.encontrarSenha();
-    }
-}
